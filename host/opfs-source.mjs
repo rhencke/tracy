5f74a605b9c84f49738cabbeac3a5e309eee284d
Fido Can Code <190991155+FidoCanCode@users.noreply.github.com>
2026-05-07 01:04:02 +0000

    Table-drive OPFS host import mappings
    Helped-by: Robert Hencke <178348+rhencke@users.noreply.github.com>

--- host/opfs-source.mjs
+++ host/opfs-source.mjs
@@ -7,6 +7,49 @@ function makeUnsupportedHostImport(name, reason) {
   };
 }
 
+function makeHostImports(source, importNames) {
+  return Object.fromEntries(importNames.map((name) => [name, source[name]]));
+}
+
+const OPFS_SOURCE_IMPORTS = Object.freeze([
+  HOST_IMPORT_NAME.OPFS_CREATE_FROM_FILE,
+  HOST_IMPORT_NAME.OPFS_READ_CHUNK,
+  HOST_IMPORT_NAME.OPFS_SOURCE_FROM_FILE,
+  HOST_IMPORT_NAME.OPFS_SOURCE_NAME,
+  HOST_IMPORT_NAME.OPFS_SOURCE_NAME_LEN,
+  HOST_IMPORT_NAME.OPFS_SOURCE_OPEN,
+  HOST_IMPORT_NAME.OPFS_SOURCE_READ,
+  HOST_IMPORT_NAME.OPFS_SOURCE_SIZE,
+]);
+
+const OPFS_INDEX_READER_IMPORTS = Object.freeze([
+  HOST_IMPORT_NAME.OPFS_INDEX_OPEN,
+  HOST_IMPORT_NAME.OPFS_INDEX_READ,
+  HOST_IMPORT_NAME.OPFS_INDEX_SIZE,
+]);
+
+const OPFS_INDEX_WRITER_IMPORTS = Object.freeze([
+  HOST_IMPORT_NAME.OPFS_INDEX_CREATE,
+  HOST_IMPORT_NAME.OPFS_INDEX_FLUSH,
+  HOST_IMPORT_NAME.OPFS_INDEX_WRITE,
+]);
+
+const OPFS_MAIN_IMPORTS = Object.freeze([
+  ...OPFS_SOURCE_IMPORTS,
+  ...OPFS_INDEX_READER_IMPORTS,
+]);
+
+const OPFS_WORKER_IMPORTS = Object.freeze([
+  ...OPFS_SOURCE_IMPORTS,
+  ...OPFS_INDEX_READER_IMPORTS,
+  ...OPFS_INDEX_WRITER_IMPORTS,
+]);
+
+const WORKER_UNSUPPORTED_FILE_IMPORTS = Object.freeze([
+  HOST_IMPORT_NAME.OPFS_CREATE_FROM_FILE,
+  HOST_IMPORT_NAME.OPFS_SOURCE_FROM_FILE,
+]);
+
 export function makeOpfsSourceHost(memoryView, files = new Map()) {
   const sources = new Map();
   const indexes = new Map();
@@ -342,7 +385,7 @@ export function makeOpfsSourceHost(memoryView, files = new Map()) {
     }
   }
 
-  const sourceHost = {
+  const opfsHost = {
     [HOST_IMPORT_NAME.OPFS_CREATE_FROM_FILE]: opfsSourceFromFile,
     [HOST_IMPORT_NAME.OPFS_READ_CHUNK]: opfsSourceRead,
     [HOST_IMPORT_NAME.OPFS_SOURCE_FROM_FILE]: opfsSourceFromFile,
@@ -351,68 +394,33 @@ export function makeOpfsSourceHost(memoryView, files = new Map()) {
     [HOST_IMPORT_NAME.OPFS_SOURCE_OPEN]: opfsSourceOpen,
     [HOST_IMPORT_NAME.OPFS_SOURCE_READ]: opfsSourceRead,
     [HOST_IMPORT_NAME.OPFS_SOURCE_SIZE]: opfsSourceSize,
-  };
-
-  const indexReaderHost = {
     [HOST_IMPORT_NAME.OPFS_INDEX_OPEN]: opfsIndexOpen,
     [HOST_IMPORT_NAME.OPFS_INDEX_READ]: opfsIndexRead,
     [HOST_IMPORT_NAME.OPFS_INDEX_SIZE]: opfsIndexSize,
-  };
-
-  const indexWriterHost = {
     [HOST_IMPORT_NAME.OPFS_INDEX_CREATE]: opfsIndexCreate,
     [HOST_IMPORT_NAME.OPFS_INDEX_FLUSH]: opfsIndexFlush,
     [HOST_IMPORT_NAME.OPFS_INDEX_WRITE]: opfsIndexWrite,
   };
 
-  return {
-    ...sourceHost,
-    ...indexReaderHost,
-    ...indexWriterHost,
-  };
+  return makeHostImports(opfsHost, OPFS_WORKER_IMPORTS);
 }
 
 export function makeOpfsMainHost(memoryView, files = new Map()) {
   const opfsHost = makeOpfsSourceHost(memoryView, files);
 
-  return {
-    [HOST_IMPORT_NAME.OPFS_CREATE_FROM_FILE]: opfsHost[HOST_IMPORT_NAME.OPFS_CREATE_FROM_FILE],
-    [HOST_IMPORT_NAME.OPFS_READ_CHUNK]: opfsHost[HOST_IMPORT_NAME.OPFS_READ_CHUNK],
-    [HOST_IMPORT_NAME.OPFS_SOURCE_FROM_FILE]: opfsHost[HOST_IMPORT_NAME.OPFS_SOURCE_FROM_FILE],
-    [HOST_IMPORT_NAME.OPFS_SOURCE_NAME]: opfsHost[HOST_IMPORT_NAME.OPFS_SOURCE_NAME],
-    [HOST_IMPORT_NAME.OPFS_SOURCE_NAME_LEN]: opfsHost[HOST_IMPORT_NAME.OPFS_SOURCE_NAME_LEN],
-    [HOST_IMPORT_NAME.OPFS_SOURCE_OPEN]: opfsHost[HOST_IMPORT_NAME.OPFS_SOURCE_OPEN],
-    [HOST_IMPORT_NAME.OPFS_SOURCE_READ]: opfsHost[HOST_IMPORT_NAME.OPFS_SOURCE_READ],
-    [HOST_IMPORT_NAME.OPFS_SOURCE_SIZE]: opfsHost[HOST_IMPORT_NAME.OPFS_SOURCE_SIZE],
-    [HOST_IMPORT_NAME.OPFS_INDEX_OPEN]: opfsHost[HOST_IMPORT_NAME.OPFS_INDEX_OPEN],
-    [HOST_IMPORT_NAME.OPFS_INDEX_READ]: opfsHost[HOST_IMPORT_NAME.OPFS_INDEX_READ],
-    [HOST_IMPORT_NAME.OPFS_INDEX_SIZE]: opfsHost[HOST_IMPORT_NAME.OPFS_INDEX_SIZE],
-  };
+  return makeHostImports(opfsHost, OPFS_MAIN_IMPORTS);
 }
 
 export function makeOpfsWorkerHost(memoryView) {
   const opfsHost = makeOpfsSourceHost(memoryView);
+  const workerHost = makeHostImports(opfsHost, OPFS_WORKER_IMPORTS);
 
-  return {
-    [HOST_IMPORT_NAME.OPFS_CREATE_FROM_FILE]: makeUnsupportedHostImport(
-      HOST_IMPORT_NAME.OPFS_CREATE_FROM_FILE,
+  for (const name of WORKER_UNSUPPORTED_FILE_IMPORTS) {
+    workerHost[name] = makeUnsupportedHostImport(
+      name,
       "file handles are owned by the main thread",
-    ),
-    [HOST_IMPORT_NAME.OPFS_READ_CHUNK]: opfsHost[HOST_IMPORT_NAME.OPFS_READ_CHUNK],
-    [HOST_IMPORT_NAME.OPFS_SOURCE_FROM_FILE]: makeUnsupportedHostImport(
-      HOST_IMPORT_NAME.OPFS_SOURCE_FROM_FILE,
-      "file handles are owned by the main thread",
-    ),
-    [HOST_IMPORT_NAME.OPFS_SOURCE_NAME]: opfsHost[HOST_IMPORT_NAME.OPFS_SOURCE_NAME],
-    [HOST_IMPORT_NAME.OPFS_SOURCE_NAME_LEN]: opfsHost[HOST_IMPORT_NAME.OPFS_SOURCE_NAME_LEN],
-    [HOST_IMPORT_NAME.OPFS_SOURCE_OPEN]: opfsHost[HOST_IMPORT_NAME.OPFS_SOURCE_OPEN],
-    [HOST_IMPORT_NAME.OPFS_SOURCE_READ]: opfsHost[HOST_IMPORT_NAME.OPFS_SOURCE_READ],
-    [HOST_IMPORT_NAME.OPFS_SOURCE_SIZE]: opfsHost[HOST_IMPORT_NAME.OPFS_SOURCE_SIZE],
-    [HOST_IMPORT_NAME.OPFS_INDEX_CREATE]: opfsHost[HOST_IMPORT_NAME.OPFS_INDEX_CREATE],
-    [HOST_IMPORT_NAME.OPFS_INDEX_FLUSH]: opfsHost[HOST_IMPORT_NAME.OPFS_INDEX_FLUSH],
-    [HOST_IMPORT_NAME.OPFS_INDEX_OPEN]: opfsHost[HOST_IMPORT_NAME.OPFS_INDEX_OPEN],
-    [HOST_IMPORT_NAME.OPFS_INDEX_READ]: opfsHost[HOST_IMPORT_NAME.OPFS_INDEX_READ],
-    [HOST_IMPORT_NAME.OPFS_INDEX_SIZE]: opfsHost[HOST_IMPORT_NAME.OPFS_INDEX_SIZE],
-    [HOST_IMPORT_NAME.OPFS_INDEX_WRITE]: opfsHost[HOST_IMPORT_NAME.OPFS_INDEX_WRITE],
-  };
+    );
+  }
+
+  return workerHost;
 }
